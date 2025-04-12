a19ae247c361a37b66ac24f148e608ecc6001704
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2025-04-12 13:23:09 -0500

    Fix crash when receiving non ipv4 headers

--- utils/src/server/mod.rs
+++ utils/src/server/mod.rs
@@ -22,35 +22,34 @@ pub fn accept<'a>(socket: &'a OwnedFd, src_port: &str) -> anyhow::Result<Connect
                 .with_context(|| "Failed to read broadcast bytes to buffer")?;
         let broadcast = &broadcast_buf[0..bytes_read];
 
-        let (_, broadcast) = etherparse::Ipv4Header::from_slice(broadcast)
-            .with_context(|| "Failed to construct header from buffer")?;
+        if let Ok((_, broadcast)) = etherparse::Ipv4Header::from_slice(broadcast) {
+            let broadcast_str = String::from_utf8_lossy(&broadcast);
 
-        let broadcast_str = String::from_utf8_lossy(&broadcast);
+            let (port, client_port) = broadcast_str
+                .split_once("::")
+                .with_context(|| "Failed to extract ip addr from port")?;
 
-        let (port, client_port) = broadcast_str
-            .split_once("::")
-            .with_context(|| "Failed to extract ip addr from port")?;
+            if src_port == port {
+                if let Some(client_addr) = sock_addr {
+                    sendto(
+                        &socket,
+                        format!("{}::{}", &client_port, &src_port).as_bytes(),
+                        rustix::net::SendFlags::empty(),
+                        &client_addr,
+                    )?;
 
-        if src_port == port {
-            if let Some(client_addr) = sock_addr {
-                sendto(
-                    &socket,
-                    format!("{}::{}", &client_port, &src_port).as_bytes(),
-                    rustix::net::SendFlags::empty(),
-                    &client_addr,
-                )?;
-
-                return Ok(Connection {
-                    conn_type: ConnectionType::Server,
-                    src_socket: socket,
-                    src_port: src_port
-                        .parse()
-                        .with_context(|| "Failed to convert port to u16")?,
-                    dst_socket: SocketAddr::try_from(client_addr)?,
-                    dst_port: client_port
-                        .parse()
-                        .with_context(|| "Failed to convert port to u16")?,
-                });
+                    return Ok(Connection {
+                        conn_type: ConnectionType::Server,
+                        src_socket: socket,
+                        src_port: src_port
+                            .parse()
+                            .with_context(|| "Failed to convert port to u16")?,
+                        dst_socket: SocketAddr::try_from(client_addr)?,
+                        dst_port: client_port
+                            .parse()
+                            .with_context(|| "Failed to convert port to u16")?,
+                    });
+                }
             }
         }
     }
